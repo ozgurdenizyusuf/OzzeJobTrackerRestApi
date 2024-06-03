1ca73c1ceaa1c077a8b3f4ed6aaaf6393e4ea2da
ozgurdenizyusuf <98545038+ozgurdenizyusuf@users.noreply.github.com>
2024-06-03 08:17:14 +0300

    Cari Hesap modeli eklendi

---
+++ Models/CariHesap.cs
@@ -0,0 +1,13 @@
+// Models/CariHesap.cs
+namespace OzzeJobTrackerRestApi.Models
+{
+    public class CariHesap
+    {
+        public string LogicalReference { get; set; }
+        public string Kod { get; set; }
+        public string Unvan { get; set; }
+        public string Adres { get; set; }
+        public string Telefon { get; set; }
+        public string Email { get; set; }
+    }
+}
