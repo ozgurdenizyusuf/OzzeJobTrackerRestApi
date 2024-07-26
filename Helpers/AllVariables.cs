1ac21e91b81df4d116dec4f9661ad2294a7a95a2
ozgurdenizyusuf <yusuf.tuncdemir@ozzegroup.kz>
2024-07-26 16:42:44 +0500

    26.07.2024

--- Helpers/AllVariables.cs
+++ Helpers/AllVariables.cs
@@ -2,6 +2,6 @@
 {
     public class AllVariables
     {
-        public static string ConnectionString = "Server=WIN-516OJB5LQLC;Database=Lbs_db_FC;User Id=sa;Password=@$Sa4257;";
+        public static string ConnectionString = "Server=OZZEGROUP-TIGER;Database=Lbs_db_FC;User Id=sa;Password=xyfJVS3wv6aqPKNHx772;";
     }
 }
